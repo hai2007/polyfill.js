46d8024651dd07e77379516f6e48a3046b2e954a
你好2007 <yelloxing@gmail.com>
2021-03-09 09:46:23 +0800

    添加banner

--- dist/polyfill.js
+++ dist/polyfill.js
@@ -1,3 +1,16 @@
+/*!
+ * ✔️ polyfill.js - ✔️ 为旧浏览器提供一些常用的原生新功能支持。
+ * git+https://github.com/hai2007/polyfill.js.git
+ *
+ * author 你好2007 < https://hai2007.gitee.io/sweethome >
+ *
+ * version 0.0.0
+ *
+ * Copyright (c) 2021-present hai2007 走一步，再走一步。
+ * Released under the MIT license
+ *
+ * Date:Tue Mar 09 2021 09:44:27 GMT+0800 (GMT+08:00)
+ */
 (function () {
 	'use strict';
 
